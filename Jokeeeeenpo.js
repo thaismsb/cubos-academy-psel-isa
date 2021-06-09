d4281246c245a09ee7025a7c11cff44072fb69ca
Thais Barbosa <thaismsbrasil@hotmail.com>
2021-06-08 21:58:35 -0300

    Faca dar certo

---
+++ Jokeeeeenpo.js
@@ -0,0 +1,26 @@
+let joao=['PAPEL'];
+
+let andre=['PAPEL'];
+
+function solucao(joao, andre) {
+    
+    if(joao=="PEDRA" && andre=="PEDRA"){
+      console.log("EMPATE")   
+}else if(joao=="PEDRA" && andre=="TESOURA"){
+  console.log("JOAO")
+}else if(joao=="PEDRA" && andre=="PAPEL"){
+  console.log("ANDRE")
+}else if(joao=="PAPEL" && andre=="PAPEL"){
+  console.log("EMPATE")
+}else if(joao=="PAPEL" && andre=="TESOURA"){
+  console.log("ANDRE")
+}else if(joao=="PAPEL" && andre=="PEDRA"){
+  console.log("JOAO")
+}else if(joao=="TESOURA" && andre=="PAPEL"){
+  console.log("JOAO")
+}else if(joao=="TESOURA" && andre=="PEDRA"){
+  console.log("ANDRE")
+}else if(joao=="TESOURA" && andre=="TESOURA"){
+  console.log("EMPATE")
+}
+}
