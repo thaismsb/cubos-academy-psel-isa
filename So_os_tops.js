66b839be3fd2f5a26f933782037e0be1044fc20f
Thais Barbosa <thaismsbrasil@hotmail.com>
2021-06-08 22:11:15 -0300

    Nao despreze os comecos pequenos

---
+++ So_os_tops.js
@@ -0,0 +1,17 @@
+const produtos= [
+    {
+      "nome": "Camiseta",
+      "preco": 7000
+    },
+    {
+      "nome": "Tenis",
+      "preco": 8000
+    },
+    {
+      "nome": "Relogio",
+      "preco": 15000
+    }
+  ];
+  
+  let totalgeral= 0;
+  let totalprodtop=0;
